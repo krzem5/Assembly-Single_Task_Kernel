0baecf7818d7eedc5230dcfdef30fe84447bbe35
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-07-23 17:06:20 +0200

    Push Update 23/07/2023, 17:06:20

--- src/user/loader/main.c
+++ src/user/loader/main.c
@@ -12,9 +12,11 @@ void main(void){
 	if (drives[drive_boot_index].type==DRIVE_TYPE_ATA||drives[drive_boot_index].type==DRIVE_TYPE_ATAPI){
 		printf("Booting from an installable medium!\n");
 	}
+	printf("Drives:\n");
 	for (u32 i=0;i<drive_count;i++){
 		printf("[%u]: %s (%s)%s\n",i,drives[i].name,drives[i].model_number,((drives[i].flags&DRIVE_FLAG_BOOT)?" [boot]":""));
 	}
+	printf("Partitions:\n");
 	for (u32 i=0;i<partition_count;i++){
 		printf("[%u]: %s%s\n",i,partitions[i].name,((partitions[i].flags&PARTITION_FLAG_BOOT)?" [boot]":""));
 	}
