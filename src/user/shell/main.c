c45fe0951458a98a3ae77dd1735819e6315170ea
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-07-26 17:15:59 +0200

    Push Update 26/07/2023, 17:15:59

--- src/user/shell/main.c
+++ src/user/shell/main.c
@@ -2,6 +2,17 @@
 
 
 
+char cwd[4096]="/";
+
+
+
+static void _print_line_start(void){
+	printf("\x1b[1m\x1b[38;2;67;154;6mshell\x1b[0m:\x1b[1m\x1b[38;2;52;101;164m%s\x1b[0m$ \n",cwd);
+}
+
+
+
 void main(void){
 	printf("Starting shell...\n");
+	_print_line_start();
 }
