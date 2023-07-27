3b156c76ea06a0ef7a9236788df3a82d73894a9e
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-07-27 10:05:47 +0200

    Push Update 27/07/2023, 10:05:47

---
+++ src/user/shell/commands/stat.c
@@ -0,0 +1,42 @@
+#include <command.h>
+#include <user/fs.h>
+#include <user/io.h>
+#include <user/partition.h>
+
+
+
+static const char* fs_stat_type_names[]={
+	[FS_STAT_TYPE_FILE]="file",
+	[FS_STAT_TYPE_DIRECTORY]="directory"
+};
+
+
+
+void stat_main(int argc,const char*const* argv){
+	if (argc<2){
+		printf("stat: no input file supplied\n");
+		return;
+	}
+	if (argc>2){
+		printf("stat: unrecognized option '%s'\n",argv[2]);
+		return;
+	}
+	int fd=fs_open(argv[1],0);
+	if (fd<0){
+		printf("stat: unable to open file '%s': error %d\n",argv[1],fd);
+		return;
+	}
+	fs_stat_t stat;
+	int error=fs_stat(fd,&stat);
+	if (error<0){
+		printf("stat: unable to read data from file '%s': error %d\n",argv[1],error);
+		goto _cleanup;
+	}
+	printf("Name: \x1b[1m%s\x1b[0m\nType: \x1b[1m%s\x1b[0m\nSize: \x1b[1m%v\x1b[0m\nPartition: \x1b[1m%s\x1b[0m\nID: \x1b[1m%p\x1b[0m\n",stat.name,fs_stat_type_names[stat.type],stat.size,(partitions+stat.fs_index)->name,stat.node_id);
+_cleanup:
+	fs_close(fd);
+}
+
+
+
+DECLARE_COMMAND(stat,"stat <file>");
