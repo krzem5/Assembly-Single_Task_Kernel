525c43d6d9c4c2216a9943b866d6636554dbf6ca
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-08-01 12:00:25 +0200

    Push Update 01/08/2023, 12:00:25

--- src/user/shell/commands/memory.c
+++ src/user/shell/commands/memory.c
@@ -15,34 +15,21 @@ void memory_main(int argc,const char*const* argv){
 		printf("memory: unable to read memory stats\n");
 		return;
 	}
-	printf("Total: \x1b[1m%v\x1b[0m\n",stats.counter_total<<12);
-	printf("Drive:\n  Drive list: \x1b[1m%v\x1b[0m\n  AHCI: \x1b[1m%v\x1b[0m\n",
-		stats.counter_drive_list<<12,
-		stats.counter_driver_ahci<<12
-	);
-	printf("FS:\n  FS: \x1b[1m%v\x1b[0m\n  FD: \x1b[1m%v\x1b[0m\n  Node allocator: \x1b[1m%v\x1b[0m\n  KFS: \x1b[1m%v\x1b[0m\n",
-		stats.counter_fs<<12,
-		stats.counter_fd<<12,
-		stats.counter_kfs<<12
-	);
-	printf("Network:\n  Network: \x1b[1m%v\x1b[0m\n  i82540: \x1b[1m%v\x1b[0m\n",
-		stats.counter_network<<12,
-		stats.counter_driver_i82540<<12
-	);
-	printf("Memory:\n  PMM: \x1b[1m%v\x1b[0m\n  VMM: \x1b[1m%v\x1b[0m\n",
-		stats.counter_pmm<<12,
-		stats.counter_vmm<<12
-	);
-	printf("User:\n  ELF: \x1b[1m%v\x1b[0m\n  MMAP: \x1b[1m%v\x1b[0m\n  Stack: \x1b[1m%v\x1b[0m\n",
-		stats.counter_elf<<12,
-		stats.counter_mmap<<12,
-		stats.counter_user_stack<<12
-	);
-	printf("Kernel:\n  Core: \x1b[1m%v\x1b[0m\n  Full: \x1b[1m%v\x1b[0m\n  Stack: \x1b[1m%v\x1b[0m\n",
-		0,
-		0,
-		stats.counter_kernel_stack<<12
-	);
+	printf("Total:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_total<<12);
+	printf("AHCI:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_driver_ahci<<12);
+	printf("Drive list:\t\x1b[1m%v\x1b[0m\n",stats.counter_drive_list<<12);
+	printf("ELF:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_elf<<12);
+	printf("FD:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_fd<<12);
+	printf("FS:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_fs<<12);
+	printf("i82540:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_driver_i82540<<12);
+	printf("Kernel sstack:\t\x1b[1m%v\x1b[0m\n",stats.counter_kernel_stack<<12);
+	printf("KFS:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_kfs<<12);
+	printf("MMAP:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_mmap<<12);
+	printf("Network:\t\x1b[1m%v\x1b[0m\n",stats.counter_network<<12);
+	printf("Node allocator:\t\x1b[1m%v\x1b[0m\n",stats.counter_node_allocator<<12);
+	printf("PMM:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_pmm<<12);
+	printf("User stack:\t\x1b[1m%v\x1b[0m\n",stats.counter_user_stack<<12);
+	printf("VMM:\t\t\x1b[1m%v\x1b[0m\n",stats.counter_vmm<<12);
 }
 
 
