0e3f92d9c2596784b09d3488f2283b4ff850009e
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-08-06 09:30:18 +0200

    Push Update 06/08/2023, 09:30:18

--- src/kernel/idt/idt.asm
+++ src/kernel/idt/idt.asm
@@ -1,6 +1,5 @@
 extern _idt_data
 global idt_enable
-global idt_set_entry
 section .text
 
 
@@ -10,27 +9,6 @@ idt_enable:
 	mov rax, qword [_idt_data]
 	mov qword [idt_pointer._data], rax
 	lidt [idt_pointer]
-	ret
-
-
-
-idt_set_entry:
-	mov r8, qword [_idt_data]
-	shl rdi, 4
-	mov rax, rsi
-	shr rax, 32
-	mov qword [r8+rdi+8], rax
-	mov rax, rsi
-	and eax, 0x0000ffff
-	and esi, 0xffff0000
-	shl rsi, 32
-	or rax, rsi
-	shl rdx, 32
-	or rax, rdx
-	shl rcx, 40
-	or rax, rcx
-	or rax, 0x00080000
-	mov qword [r8+rdi], rax
 	ret
 
 
--- src/kernel/idt/idt.c
+++ src/kernel/idt/idt.c
@@ -7,7 +7,7 @@
 
 
 
-void* _idt_data;
+u64* _idt_data;
 
 
 
@@ -20,4 +20,8 @@ void idt_init(void){
 
 
 
-void idt_set_entry(u8 index,void* callback,u8 ist,u8 flags);
+void idt_set_entry(u8 index,void* callback,u8 ist,u8 flags){
+	u64 callback_address=(u64)callback;
+	_idt_data[index<<1]=((callback_address&0xffff0000)<<32)|(((u64)flags)<<40)|(((u64)ist)<<32)|0x00080000|(callback_address&0x0000ffff);
+	_idt_data[(index<<1)|1]=callback_address>>32;
+}
