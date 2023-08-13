530e7ab3a2afb749110d0b10cad5d70be02b585c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-08-13 11:59:45 +0200

    Push Update 13/08/2023, 11:59:45

--- src/kernel/idt/idt.asm
+++ src/kernel/idt/idt.asm
@@ -1,5 +1,5 @@
-global _idt_data
 global idt_enable
+global idt_set_entry
 section .text
 
 
@@ -11,6 +11,26 @@ idt_enable:
 
 
 
+idt_set_entry:
+	shl rdi, 4
+	mov rax, rsi
+	shr rax, 32
+	mov qword [idt_data+rdi+8], rax
+	mov rax, rsi
+	and eax, 0x0000ffff
+	and esi, 0xffff0000
+	shl rsi, 32
+	or rax, rsi
+	shl rdx, 32
+	or rax, rdx
+	shl rcx, 40
+	or rax, rcx
+	or rax, 0x00080000
+	mov qword [idt_data+rdi], rax
+	ret
+
+
+
 section .data
 
 
@@ -18,8 +38,7 @@ section .data
 align 16
 idt_pointer:
 	dw 0x0fff
-._data:
-	dq _idt_data
+	dq idt_data
 
 
 
@@ -28,5 +47,5 @@ section .common
 
 
 align 8
-_idt_data:
+idt_data:
 	times 512 dq 0
