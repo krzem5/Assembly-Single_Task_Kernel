b67171966ac7e407143aab1f325f65f4523676b4
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-08-20 21:02:16 +0200

    Push Update 20/08/2023, 21:02:16

--- src/user/shell/commands/numa.c
+++ src/user/shell/commands/numa.c
@@ -9,7 +9,12 @@ void numa_main(int argc,const char*const* argv){
 		printf("numa: unrecognized option '%s'\n",argv[1]);
 		return;
 	}
-	printf("NUMA node count: %u\n",numa_node_count);
+	printf("NUMA node count: \x1b[1m%u\x1b[0m\nNUMA locality matrix:\n",numa_node_count);
+	for (u32 i=0;i<numa_node_count;i++){
+		for (u32 j=0;j<numa_node_count;j++){
+			printf("[%u->%u] \x1b[1m%u\x1b[0m\n",i,j,numa_node_locality_matrix[i*numa_node_count+j]);
+		}
+	}
 }
 
 
