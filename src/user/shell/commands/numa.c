560d43f694a0c0f6ec2fc143cd04d49aa84f30a8
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-08-21 10:55:33 +0200

    Push Update 21/08/2023, 10:55:33

--- src/user/shell/commands/numa.c
+++ src/user/shell/commands/numa.c
@@ -12,7 +12,7 @@ void numa_main(int argc,const char*const* argv){
 	printf("NUMA node count: \x1b[1m%u\x1b[0m\nNUMA locality matrix:\n",numa_node_count);
 	for (u32 i=0;i<numa_node_count;i++){
 		for (u32 j=0;j<numa_node_count;j++){
-			printf("[%u->%u] \x1b[1m%u\x1b[0m\n",i,j,numa_node_locality_matrix[i*numa_node_count+j]);
+			printf("[%u,%u] \x1b[1m%u\x1b[0m\n",i,j,numa_node_locality_matrix[i*numa_node_count+j]);
 		}
 	}
 }
