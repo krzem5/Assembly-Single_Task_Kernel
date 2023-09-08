725b7a265bf41dbdfe77186c0229a4716a321e8c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-09-08 13:10:47 +0200

    Push Update 08/09/2023, 13:10:47

--- src/kernel/topology/topology.asm
+++ src/kernel/topology/topology.asm
@@ -18,6 +18,8 @@ topology_get_cpu_bits:
 	jge ._intel_11
 	cmp eax, 4
 	jge ._intel_04
+	cmp eax, 1
+	jge ._intel_01
 ._error:
 	xor rax, rax
 	pop rbx
@@ -36,5 +38,31 @@ topology_get_cpu_bits:
 	pop rbx
 	ret
 ._intel_04:
+	mov eax, 4
+	cpuid
+	shr eax, 26
+	bsr r8d, eax
+	add r8, 1
+	mov eax, 1
+	cpuid
+	shr eax, 16
+	sub eax, 1
+	and eax, 0xff
+	bsr eax, eax
+	add eax, 1
+	sub rax, r8
+	shl r8, 32
+	or rax, r8
+	pop rbx
+	ret
+._intel_01:
+	mov eax, 1
+	cpuid
+	mov eax, ebx
+	shr eax, 16
+	sub eax, 1
+	and eax, 0xff
+	bsr eax, eax
+	add eax, 1
 	pop rbx
 	ret
