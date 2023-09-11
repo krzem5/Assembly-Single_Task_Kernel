66edd0b815c3338a18a6249104467a9bb24e2f47
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-09-11 21:04:49 +0200

    Push Update 11/09/2023, 21:04:49

--- src/kernel/isr/isr.c
+++ src/kernel/isr/isr.c
@@ -1,33 +1,7 @@
+#include <kernel/gpr/gpr.h>
 #include <kernel/log/log.h>
 #include <kernel/types.h>
 #define KERNEL_LOG_NAME "isr"
-
-
-
-typedef struct _GPR_STATE{
-	u64 rax;
-	u64 rbx;
-	u64 rcx;
-	u64 rdx;
-	u64 rsi;
-	u64 rdi;
-	u64 rbp;
-	u64 r8;
-	u64 r9;
-	u64 r10;
-	u64 r11;
-	u64 r12;
-	u64 r13;
-	u64 r14;
-	u64 r15;
-	u64 isr;
-	u64 error;
-	u64 rip;
-	u64 cs;
-	u64 rflags;
-	u64 rsp;
-	u64 ss;
-} gpr_state_t;
 
 
 
