008a38555c29177fcc84c12be8ea21593c5c392f
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-09-13 18:20:06 +0200

    Push Update 13/09/2023, 18:20:06

--- src/kernel/isr/isr.c
+++ src/kernel/isr/isr.c
@@ -1,4 +1,4 @@
-#include <kernel/gpr/gpr.h>
+#include <kernel/isr/isr.h>
 #include <kernel/log/log.h>
 #include <kernel/types.h>
 #include <kernel/util/util.h>
@@ -6,32 +6,32 @@
 
 
 
-void _isr_handler(gpr_state_t* gpr_state){
-	WARN("ISR %u:",gpr_state->isr);
-	WARN("cr3    = %p",gpr_state->cr3);
-	WARN("es     = %p",gpr_state->es);
-	WARN("ds     = %p",gpr_state->ds);
-	WARN("rax    = %p",gpr_state->rax);
-	WARN("rbx    = %p",gpr_state->rbx);
-	WARN("rcx    = %p",gpr_state->rcx);
-	WARN("rdx    = %p",gpr_state->rdx);
-	WARN("rsi    = %p",gpr_state->rsi);
-	WARN("rdi    = %p",gpr_state->rdi);
-	WARN("rbp    = %p",gpr_state->rbp);
-	WARN("r8     = %p",gpr_state->r8);
-	WARN("r9     = %p",gpr_state->r9);
-	WARN("r10    = %p",gpr_state->r10);
-	WARN("r11    = %p",gpr_state->r11);
-	WARN("r12    = %p",gpr_state->r12);
-	WARN("r13    = %p",gpr_state->r13);
-	WARN("r14    = %p",gpr_state->r14);
-	WARN("r15    = %p",gpr_state->r15);
-	WARN("error  = %p",gpr_state->error);
-	WARN("rip    = %p",gpr_state->rip);
-	WARN("cs     = %p",gpr_state->cs);
-	WARN("rflags = %p",gpr_state->rflags);
-	WARN("rsp    = %p",gpr_state->rsp);
-	WARN("ss     = %p",gpr_state->ss);
+void _isr_handler(isr_state_t* isr_state){
+	WARN("ISR %u:",isr_state->isr);
+	WARN("cr3    = %p",isr_state->cr3);
+	WARN("es     = %p",isr_state->es);
+	WARN("ds     = %p",isr_state->ds);
+	WARN("rax    = %p",isr_state->rax);
+	WARN("rbx    = %p",isr_state->rbx);
+	WARN("rcx    = %p",isr_state->rcx);
+	WARN("rdx    = %p",isr_state->rdx);
+	WARN("rsi    = %p",isr_state->rsi);
+	WARN("rdi    = %p",isr_state->rdi);
+	WARN("rbp    = %p",isr_state->rbp);
+	WARN("r8     = %p",isr_state->r8);
+	WARN("r9     = %p",isr_state->r9);
+	WARN("r10    = %p",isr_state->r10);
+	WARN("r11    = %p",isr_state->r11);
+	WARN("r12    = %p",isr_state->r12);
+	WARN("r13    = %p",isr_state->r13);
+	WARN("r14    = %p",isr_state->r14);
+	WARN("r15    = %p",isr_state->r15);
+	WARN("error  = %p",isr_state->error);
+	WARN("rip    = %p",isr_state->rip);
+	WARN("cs     = %p",isr_state->cs);
+	WARN("rflags = %p",isr_state->rflags);
+	WARN("rsp    = %p",isr_state->rsp);
+	WARN("ss     = %p",isr_state->ss);
 	for (;;);
 }
 
