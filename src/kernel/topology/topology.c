424f43347bcc429541baef5772550051e2126f6c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-09-25 19:26:44 +0200

    Push Update 25/09/2023, 19:26:44

--- src/kernel/topology/topology.c
+++ src/kernel/topology/topology.c
@@ -5,19 +5,12 @@
 
 
 
-static u32 _topology_thread_bits=0xffffffff;
-static u32 _topology_core_bits;
-
-
-
 void topology_compute(u8 apic_id,topology_t* out){
-	if (_topology_thread_bits==0xffffffff){
-		u64 bits=topology_get_cpu_bits();
-		_topology_thread_bits=bits;
-		_topology_core_bits=bits>>32;
-	}
-	out->thread=apic_id&((1<<_topology_thread_bits)-1);
-	out->core=(apic_id>>_topology_thread_bits)&((1<<_topology_core_bits)-1);
-	out->chip=apic_id>>(_topology_thread_bits+_topology_core_bits);
+	u64 bits=topology_get_cpu_bits();
+	u32 thread_bits=bits;
+	u32 core_bits=bits>>32;
+	out->thread=apic_id&((1<<thread_bits)-1);
+	out->core=(apic_id>>thread_bits)&((1<<core_bits)-1);
+	out->chip=apic_id>>(thread_bits+core_bits);
 	out->domain=0;
 }
