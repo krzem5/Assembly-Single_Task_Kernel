eae74eaf6fe7f8b3269d8c6ebd8346a6dc0d8896
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-09-25 08:27:43 +0200

    Push Update 25/09/2023, 08:27:43

--- build.py
+++ build.py
@@ -158,12 +158,23 @@ def _read_kernel_symbols(file_path):
 
 
 
-def _read_kernel_object_file_symbol_names(file_name,file_path,out):
-	for line in subprocess.run(["nm","-f","bsd","--defined-only",file_path],stdout=subprocess.PIPE).stdout.decode("utf-8").split("\n"):
-		line=line.strip().split(" ")
-		if (len(line)<3 or line[1].lower() in "vw" or line[2].startswith("__func__") or "." in line[2]):
-			continue
-		out.append(line[2])
+def _read_extracted_object_file_symbols(object_file,out):
+	with open(object_file+".syms","r") as rf:
+		for symbol in rf.read().split("\n"):
+			symbol=symbol.strip()
+			if (symbol):
+				out.append(symbol)
+
+
+
+def _extract_object_file_symbol_names(object_file,out):
+	with open(object_file+".syms","w") as wf:
+		for line in subprocess.run(["nm","-f","bsd","--defined-only",object_file],stdout=subprocess.PIPE).stdout.decode("utf-8").split("\n"):
+			line=line.strip().split(" ")
+			if (len(line)<3 or line[1].lower() in "vw" or line[2].startswith("__func__") or "." in line[2]):
+				continue
+			out.append(line[2])
+			wf.write(line[2]+"\n")
 
 
 
@@ -371,7 +382,7 @@ for root,_,files in os.walk(KERNEL_FILE_DIRECTORY):
 		object_file=KERNEL_OBJECT_FILE_DIRECTORY+file.replace("/","#")+".o"
 		object_files.append(object_file)
 		if (_file_not_changed(changed_files,object_file+".deps")):
-			_read_kernel_object_file_symbol_names(file_name,object_file,kernel_symbols)
+			_read_extracted_object_file_symbols(object_file,kernel_symbols)
 			continue
 		command=None
 		if (suffix==".c"):
@@ -381,7 +392,7 @@ for root,_,files in os.walk(KERNEL_FILE_DIRECTORY):
 		if (subprocess.run(command+["-MD","-MT",object_file,"-MF",object_file+".deps"]).returncode!=0):
 			del file_hash_list[file]
 			error=True
-		_read_kernel_object_file_symbol_names(file_name,object_file,kernel_symbols)
+		_extract_object_file_symbol_names(object_file,kernel_symbols)
 _save_file_hash_list(file_hash_list,KERNEL_HASH_FILE_PATH)
 object_files.append(_generate_symbol_file(kernel_symbols,KERNEL_SYMBOL_FILE_PATH))
 os.remove(KERNEL_VERSION_FILE_PATH)
