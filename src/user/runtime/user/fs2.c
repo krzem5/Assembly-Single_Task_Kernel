5d3213052433683deeee03b6595ab02a025fb428
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-10-18 20:14:09 +0200

    Push Update 18/10/2023, 20:14:09

--- src/user/runtime/user/fs2.c
+++ src/user/runtime/user/fs2.c
@@ -9,65 +9,65 @@ int fs2_open(int fd,const char* path,u8 flags){
 	while (path[length]){
 		length++;
 	}
-	return _syscall_fd_open(fd,path,length,flags);
+	return _syscall_fd2_open(fd,path,length,flags);
 }
 
 
 
 int fs2_close(int fd){
-	return _syscall_fd_close(fd);
+	return _syscall_fd2_close(fd);
 }
 
 
 
 int fs2_delete(int fd){
-	return _syscall_fd_delete(fd);
+	return _syscall_fd2_delete(fd);
 }
 
 
 
 s64 fs2_read(int fd,void* buffer,u64 count){
-	return _syscall_fd_read(fd,buffer,count);
+	return _syscall_fd2_read(fd,buffer,count);
 }
 
 
 
 s64 fs2_write(int fd,const void* buffer,u64 count){
-	return _syscall_fd_write(fd,buffer,count);
+	return _syscall_fd2_write(fd,buffer,count);
 }
 
 
 
 s64 fs2_seek(int fd,u64 offset,u8 type){
-	return _syscall_fd_seek(fd,offset,type);
+	return _syscall_fd2_seek(fd,offset,type);
 }
 
 
 
 int fs2_resize(int fd,u64 size){
-	return _syscall_fd_resize(fd,size);
+	return _syscall_fd2_resize(fd,size);
 }
 
 
 
 int fs2_absolute_path(int fd,char* buffer,u32 buffer_length){
-	return _syscall_fd_absolute_path(fd,buffer,buffer_length);
+	return _syscall_fd2_absolute_path(fd,buffer,buffer_length);
 }
 
 
 
 int fs2_stat(int fd,fs2_stat_t* stat){
-	return _syscall_fd_stat(fd,stat,sizeof(fs2_stat_t));
+	return _syscall_fd2_stat(fd,stat,sizeof(fs2_stat_t));
 }
 
 
 
 int fs2_get_relative(int fd,u8 relative,u8 flags){
-	return _syscall_fd_get_relative(fd,relative,flags);
+	return _syscall_fd2_get_relative(fd,relative,flags);
 }
 
 
 
 int fs2_move(int fd,int dst_fd){
-	return _syscall_fd_move(fd,dst_fd);
+	return _syscall_fd2_move(fd,dst_fd);
 }
