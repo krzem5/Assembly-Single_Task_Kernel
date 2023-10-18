d0d7dce9cb1c8d29c70faaccd1f31bb3f12dfcbf
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-10-18 20:57:01 +0200

    Push Update 18/10/2023, 20:57:01

--- src/user/shell/main.c
+++ src/user/shell/main.c
@@ -2,29 +2,11 @@
 #include <cwd.h>
 #include <input.h>
 #include <user/clock.h>
-#include <user/io.h>
-#include <user/network.h>
-#include <user/thread.h>
-
-
-
-static void _network_thread(void* arg){
-	u8 buffer[64];
-	network_packet_t packet;
-	while (1){
-		packet.buffer_length=64;
-		packet.buffer=buffer;
-		network_poll(&packet,1);
-	}
-}
-
 
 
 void main(void){
 	clock_init();
 	cwd_init();
-	u64 tid=thread_create(_network_thread,NULL,0);
-	thread_set_priority(tid,THREAD_PRIORITY_LOW);
 	while (1){
 		input_get();
 		command_execute(input);
