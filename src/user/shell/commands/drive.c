1c22459b5b08d3718994c9d8fc7e826d8deea065
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-10-21 08:56:37 +0200

    Push Update 21/10/2023, 08:56:37

--- src/user/shell/commands/drive.c
+++ src/user/shell/commands/drive.c
@@ -3,6 +3,7 @@
 #include <user/clock.h>
 #include <user/drive.h>
 #include <user/fd.h>
+#include <user/handle.h>
 #include <user/io.h>
 #include <user/memory.h>
 #include <user/types.h>
@@ -31,22 +32,28 @@ void drive_main(int argc,const char*const* argv){
 		printf("drive: no drive supplied\n");
 		return;
 	}
-	// u32 i=0;
-	// drive_t drive;
-	// for (;drive_get(i,&drive);i++){
-	// 	if (string_equal(drive.name,drive_name)){
-	// 		goto _drive_found;
-	// 	}
-	// }
+	drive_data_t data;
+	handle_iterator_t iterator;
+	HANDLE_FOREACH(&iterator,"drive"){
+		if (!drive_get_data(iterator.handle,&data)){
+			continue;
+		}
+		if (string_equal(data.name,drive_name)){
+			goto _drive_found;
+		}
+	}
 	printf("drive: drive '%s' not found\n",drive_name);
 	return;
-// _drive_found:
-// 	printf("Name: \x1b[1m%s\x1b[0m\nSize: \x1b[1m%v\x1b[0m\nBlock size: \x1b[1m%v\x1b[0m\nBlock count: \x1b[1m%lu\x1b[0m\n",
-// 		drive_name,
-// 		drive.block_count*drive.block_size,
-// 		drive.block_size,
-// 		drive.block_count
-// 	);
+_drive_found:
+	printf("Name: \x1b[1m%s\x1b[0m (%s)\nModel name: \x1b[1m%s\x1b[0m\nSerial Number: \x1b[1m%s\x1b[0m\nSize: \x1b[1m%v\x1b[0m\nBlock size: \x1b[1m%v\x1b[0m\nBlock count: \x1b[1m%lu\x1b[0m\n",
+		data.name,
+		data.type,
+		data.model_number,
+		data.serial_number,
+		data.block_count*data.block_size,
+		data.block_size,
+		data.block_count
+	);
 }
 
 
