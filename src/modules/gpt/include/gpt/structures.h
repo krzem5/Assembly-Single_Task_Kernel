257e0cc6fa3af06382a0a064e38fcd7c108bb4c7
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-10-26 15:55:37 +0200

    Push Update 26/10/2023, 15:55:37

--- src/modules/gpt/include/gpt/structures.h
+++ src/modules/gpt/include/gpt/structures.h
@@ -13,13 +13,24 @@ typedef struct _GPT_TABLE_HEADER{
 	u32 revision;
 	u32 size;
 	u8 _padding[24];
-	u64 first_block;
-	u64 last_block;
+	u64 first_lba;
+	u64 last_lba;
 	u8 guid[16];
 	u64 entry_lba;
 	u32 entry_count;
 	u32 entry_size;
 } gpt_table_header_t;
+
+
+
+typedef struct _GPT_PARTITION_ENTRY{
+	u8 type_guid[16];
+	u8 guid[16];
+	u64 start_lba;
+	u64 end_lba;
+	u64 attributes;
+	u16 name[36];
+} gpt_partition_entry_t;
 
 
 
