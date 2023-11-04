1c60a2c22df3c83116a31cd6834050d01c71fc3e
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-11-04 20:29:41 +0100

    Push Update 04/11/2023, 20:29:41

--- src/kernel/elf/elf.c
+++ src/kernel/elf/elf.c
@@ -9,6 +9,7 @@
 #include <kernel/mp/thread.h>
 #include <kernel/scheduler/scheduler.h>
 #include <kernel/types.h>
+#include <kernel/util/util.h>
 #include <kernel/vfs/node.h>
 #define KERNEL_LOG_NAME "elf"
 
@@ -23,44 +24,37 @@ _Bool elf_load(vfs_node_t* node){
 		return 0;
 	}
 	process_t* process=process_new();
-	elf_hdr_t header;
-	if (vfs_node_read(node,0,&header,sizeof(elf_hdr_t))!=sizeof(elf_hdr_t)||header.e_ident.signature!=0x464c457f||header.e_ident.word_size!=2||header.e_ident.endianess!=1||header.e_ident.header_version!=1||header.e_ident.abi!=0||header.e_type!=ET_EXEC||header.e_machine!=0x3e||header.e_version!=1){
+	mmap_region_t* region=mmap_alloc(&(process_kernel->mmap),0,0,NULL,MMAP_REGION_FLAG_NO_FILE_WRITEBACK|MMAP_REGION_FLAG_VMM_NOEXECUTE|MMAP_REGION_FLAG_VMM_READWRITE,node);
+	void* file_data=(void*)(region->rb_node.key);
+	elf_hdr_t header=*((elf_hdr_t*)file_data);
+	if (header.e_ident.signature!=0x464c457f||header.e_ident.word_size!=2||header.e_ident.endianess!=1||header.e_ident.header_version!=1||header.e_ident.abi!=0||header.e_type!=ET_EXEC||header.e_machine!=0x3e||header.e_version!=1){
 		goto _error;
 	}
-	u64 highest_address=0;
 	for (u16 i=0;i<header.e_phnum;i++){
-		elf_phdr_t program_header;
-		if (vfs_node_read(node,header.e_phoff+i*sizeof(elf_phdr_t),&program_header,sizeof(elf_phdr_t))!=sizeof(elf_phdr_t)){
-			goto _error;
-		}
-		if (program_header.p_type!=PT_LOAD){
+		const elf_phdr_t* program_header=file_data+header.e_phoff+i*sizeof(elf_phdr_t);
+		if (program_header->p_type!=PT_LOAD){
 			continue;
 		}
 		u64 flags=VMM_PAGE_FLAG_USER|VMM_PAGE_FLAG_PRESENT;
-		if (!(program_header.p_flags&PF_X)){
+		if (!(program_header->p_flags&PF_X)){
 			flags|=VMM_PAGE_FLAG_NOEXECUTE;
 		}
-		if (program_header.p_flags&PF_W){
+		if (program_header->p_flags&PF_W){
 			flags|=VMM_PAGE_FLAG_READWRITE;
 		}
-		u64 offset=program_header.p_vaddr&(PAGE_SIZE-1);
-		u64 page_count=pmm_align_up_address(program_header.p_memsz+offset)>>PAGE_SIZE_SHIFT;
+		u64 offset=program_header->p_vaddr&(PAGE_SIZE-1);
+		u64 page_count=pmm_align_up_address(program_header->p_memsz+offset)>>PAGE_SIZE_SHIFT;
 		u64 pages=pmm_alloc_zero(page_count,&_user_image_pmm_counter,0);
-		if (!mmap_alloc(&(process->mmap),program_header.p_vaddr-offset,page_count<<PAGE_SIZE_SHIFT,&_user_image_pmm_counter,0,NULL)){
+		if (!mmap_alloc(&(process->mmap),program_header->p_vaddr-offset,page_count<<PAGE_SIZE_SHIFT,&_user_image_pmm_counter,0,NULL)){
 			goto _error;
 		}
-		vmm_map_pages(&(process->pagemap),pages,program_header.p_vaddr-offset,flags,page_count);
-		u64 end_address=program_header.p_vaddr-offset+(page_count<<PAGE_SIZE_SHIFT);
-		if (end_address>highest_address){
-			highest_address=end_address;
-		}
-		if (vfs_node_read(node,program_header.p_offset,(void*)(pages+offset+VMM_HIGHER_HALF_ADDRESS_OFFSET),program_header.p_filesz)!=program_header.p_filesz){
-			goto _error;
-		}
+		vmm_map_pages(&(process->pagemap),pages,program_header->p_vaddr-offset,flags,page_count);
+		memcpy((void*)(pages+offset+VMM_HIGHER_HALF_ADDRESS_OFFSET),file_data+program_header->p_offset,program_header->p_filesz);
 	}
 	scheduler_enqueue_thread(thread_new_user_thread(process,header.e_entry,0x200000));
 	return 1;
 _error:
+	mmap_dealloc(&(process_kernel->mmap),region->rb_node.key,region->length);
 	handle_release(&(process->handle));
 	return 0;
 }
--- src/kernel/module/module.c
+++ src/kernel/module/module.c
@@ -247,12 +247,12 @@ module_t* module_load(const char* name){
 		return NULL;
 	}
 	INFO("Loading module from file...");
-	mmap_region_t* file_data_region=mmap_alloc(&(process_kernel->mmap),0,0,NULL,MMAP_REGION_FLAG_NO_FILE_WRITEBACK|MMAP_REGION_FLAG_VMM_NOEXECUTE|MMAP_REGION_FLAG_VMM_READWRITE,module_file);
-	INFO("Module file size: %v",file_data_region->length);
-	void* file_data=(void*)(file_data_region->rb_node.key);
+	mmap_region_t* region=mmap_alloc(&(process_kernel->mmap),0,0,NULL,MMAP_REGION_FLAG_NO_FILE_WRITEBACK|MMAP_REGION_FLAG_VMM_NOEXECUTE|MMAP_REGION_FLAG_VMM_READWRITE,module_file);
+	INFO("Module file size: %v",region->length);
+	void* file_data=(void*)(region->rb_node.key);
 	elf_hdr_t header=*((elf_hdr_t*)file_data);
 	if (header.e_ident.signature!=0x464c457f||header.e_ident.word_size!=2||header.e_ident.endianess!=1||header.e_ident.header_version!=1||header.e_ident.abi!=0||header.e_type!=ET_REL||header.e_machine!=0x3e||header.e_version!=1){
-		mmap_dealloc(&(process_kernel->mmap),file_data_region->rb_node.key,file_data_region->length);
+		mmap_dealloc(&(process_kernel->mmap),region->rb_node.key,region->length);
 		spinlock_release_exclusive(&_module_global_lock);
 		return NULL;
 	}
@@ -261,7 +261,7 @@ module_t* module_load(const char* name){
 	handle_new(module,HANDLE_TYPE_MODULE,&(module->handle));
 	_map_section_addresses(file_data,&header,module);
 	_apply_relocations(file_data,&header,name);
-	mmap_dealloc(&(process_kernel->mmap),file_data_region->rb_node.key,file_data_region->length);
+	mmap_dealloc(&(process_kernel->mmap),region->rb_node.key,region->length);
 	INFO("Adjusting memory flags...");
 	vmm_adjust_flags(&vmm_kernel_pagemap,module->ex_region.base,0,VMM_PAGE_FLAG_READWRITE,module->ex_region.size>>PAGE_SIZE_SHIFT);
 	vmm_adjust_flags(&vmm_kernel_pagemap,module->nx_region.base,VMM_PAGE_FLAG_NOEXECUTE,VMM_PAGE_FLAG_READWRITE,module->nx_region.size>>PAGE_SIZE_SHIFT);
