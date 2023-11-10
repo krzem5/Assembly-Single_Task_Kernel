8871c9a521a2442b84f7f1bc1cfbbc4f7098267c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-11-10 18:38:49 +0100

    Push Update 10/11/2023, 18:38:49

--- src/user/shell/commands/sched.c
+++ src/user/shell/commands/sched.c
@@ -3,7 +3,7 @@
 #include <user/clock.h>
 #include <user/cpu.h>
 #include <user/io.h>
-#include <user/scheduler.h>
+// #include <user/scheduler.h>
 
 
 
@@ -14,22 +14,22 @@ void sched_main(int argc,const char*const* argv){
 	}
 	u32 cpu_count=cpu_get_count();
 	for (u32 i=0;i<cpu_count;i++){
-		scheduler_stats_t stats;
-		if (!scheduler_get_stats(i,&stats)){
-			goto _error_stats;
-		}
-		scheduler_timers_t timers;
-		if (!scheduler_get_timers(i,&timers)){
-			goto _error_timers;
-		}
-		printf("#%u:\t%lu\t%lu\t%lu\t%lu\t%lu\n",i,stats.added_thread_count,clock_ticks_to_time(timers.timer_user)/1000000ull,clock_ticks_to_time(timers.timer_kernel)/1000000ull,clock_ticks_to_time(timers.timer_scheduler)/1000000ull,clock_ticks_to_time(timers.timer_none)/1000000ull);
+		// scheduler_stats_t stats;
+		// if (!scheduler_get_stats(i,&stats)){
+		// 	goto _error_stats;
+		// }
+		// scheduler_timers_t timers;
+		// if (!scheduler_get_timers(i,&timers)){
+		// 	goto _error_timers;
+		// }
+		// printf("#%u:\t%lu\t%lu\t%lu\t%lu\t%lu\n",i,stats.added_thread_count,clock_ticks_to_time(timers.timer_user)/1000000ull,clock_ticks_to_time(timers.timer_kernel)/1000000ull,clock_ticks_to_time(timers.timer_scheduler)/1000000ull,clock_ticks_to_time(timers.timer_none)/1000000ull);
 	}
-	return;
-_error_stats:
-	printf("sched: unable to read scheduler stats\n");
-	return;
-_error_timers:
-	printf("sched: unable to read scheduler timers\n");
+// 	return;
+// _error_stats:
+// 	printf("sched: unable to read scheduler stats\n");
+// 	return;
+// _error_timers:
+// 	printf("sched: unable to read scheduler timers\n");
 }
 
 
