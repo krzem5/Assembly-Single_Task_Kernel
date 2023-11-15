8f4f747fe71571ecefc3395fb876447e125b6627
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-11-15 11:31:07 +0100

    Push Update 15/11/2023, 11:31:07

--- src/lib/linker/main.c
+++ src/lib/linker/main.c
@@ -114,11 +114,11 @@ static void _parse_dynamic_section(const elf_dyn_t* dynamic_section,u64 image_ba
 
 
 
-static u64 _load_root_shared_object_data(const u64* data,const elf_dyn_t** out){
+static const elf_dyn_t* _load_root_shared_object_data(const u64* data,u64* out){
 	const void* phdr_entries=NULL;
 	u64 phdr_entry_size=0;
 	u64 phdr_entry_count=0;
-	u64 ret=0;
+	*out=0;
 	for (data+=(data[0]+1);data[0];data++);
 	for (data++;data[0];data+=2){
 		if (data[0]==AT_PHDR){
@@ -131,25 +131,22 @@ static u64 _load_root_shared_object_data(const u64* data,const elf_dyn_t** out){
 			phdr_entry_count=data[1];
 		}
 		else if (data[0]==AT_ENTRY){
-			ret=data[1];
+			*out=data[1];
 		}
 	}
 	if (!phdr_entries||!phdr_entry_size||!phdr_entry_count){
 		printf("No PHDR supplied to the dynamic linker\n");
 	}
-	if (!ret){
+	if (!(*out)){
 		printf("No entry address supplied to the dynamic linker\n");
 	}
-	*out=NULL;
 	for (u16 i=0;i<phdr_entry_count;i++){
 		const elf_phdr_t* program_header=phdr_entries+i*phdr_entry_size;
-		if (program_header->p_type!=PT_DYNAMIC){
-			continue;
+		if (program_header->p_type==PT_DYNAMIC){
+			return(void*)(program_header->p_vaddr);
 		}
-		*out=(void*)(program_header->p_vaddr);
-		break;
 	}
-	return ret;
+	return NULL;
 }
 
 
@@ -417,8 +414,7 @@ u64 main(const u64* data){
 	root_shared_object=_alloc_shared_object();
 	root_shared_object->next=NULL;
 	root_shared_object->image_base=0;
-	const elf_dyn_t* dynamic_section;
-	u64 entry_address=_load_root_shared_object_data(base_data,&dynamic_section);
-	_init_shared_object(root_shared_object,dynamic_section);
+	u64 entry_address;
+	_init_shared_object(root_shared_object,_load_root_shared_object_data(base_data,&entry_address));
 	return entry_address;
 }
