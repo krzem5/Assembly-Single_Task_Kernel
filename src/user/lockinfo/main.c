2e7f880e69c6e689ff31a7e135fa84fda0df925c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-11-20 10:19:38 +0100

    Push Update 20/11/2023, 10:19:38

--- src/user/lockinfo/main.c
+++ src/user/lockinfo/main.c
@@ -10,15 +10,18 @@ typedef struct _LOCK_TYPE_DATA{
 
 
 
-static _Bool _load_location(s64 fd,char* location){
-	fd=sys_fd_open(fd,"location",SYS_FD_FLAG_READ);
+static _Bool _read_file(s64 fd,const char* name,char* buffer,u32 buffer_length){
+	if (!buffer_length){
+		return 0;
+	}
+	fd=sys_fd_open(fd,name,SYS_FD_FLAG_READ);
 	if (fd<=0){
 		return 0;
 	}
-	s64 size=sys_fd_read(fd,location,47,0);
+	s64 size=sys_fd_read(fd,buffer,buffer_length-1,0);
 	sys_fd_close(fd);
 	if (size>0){
-		location[size]=0;
+		buffer[size]=0;
 	}
 	return size>0;
 }
@@ -51,39 +54,48 @@ int main(int argc,const char** argv){
 		types[i].location[0]=0;
 	}
 	for (s64 iter=sys_fd_iter_start(types_fd);iter>=0;iter=sys_fd_iter_next(iter)){
-		char name[32];
-		if (sys_fd_iter_get(iter,name,32)<=0){
+		char buffer[32];
+		if (sys_fd_iter_get(iter,buffer,32)<=0){
 			continue;
 		}
-		u32 i=sys_options_atoi(name);
+		u32 i=sys_options_atoi(buffer);
 		if (!i||i>max_type){
 			continue;
 		}
-		s64 fd=sys_fd_open(types_fd,name,0);
+		s64 fd=sys_fd_open(types_fd,buffer,0);
 		if (fd<=0){
 			continue;
 		}
-		_load_location(fd,(types+i)->location);
+		_read_file(fd,"location",(types+i)->location,48);
 		sys_fd_close(fd);
 		printf("%u: %s\n",i,(types+i)->location);
 	}
 	sys_fd_close(types_fd);
 	s64 data_fd=sys_fd_open(root_fd,"data",0);
 	for (s64 iter=sys_fd_iter_start(data_fd);iter>=0;iter=sys_fd_iter_next(iter)){
-		char name[32];
-		if (sys_fd_iter_get(iter,name,32)<=0){
+		char buffer[32];
+		if (sys_fd_iter_get(iter,buffer,32)<=0){
 			continue;
 		}
-		s64 fd=sys_fd_open(data_fd,name,0);
+		s64 fd=sys_fd_open(data_fd,buffer,0);
 		if (fd<=0){
 			continue;
 		}
 		char location[48];
-		if (!_load_location(fd,location)){
+		if (!_read_file(fd,"location",location,48)){
 			sys_fd_close(fd);
 			continue;
 		}
-		printf("%s\n",location);
+		for (s64 iter=sys_fd_iter_start(fd);iter>=0;iter=sys_fd_iter_next(iter)){
+			if (sys_fd_iter_get(iter,buffer,32)<=0||buffer[0]<48||buffer[0]>57){
+				continue;
+			}
+			u32 type=sys_options_atoi(buffer);
+			if (type>max_type){
+				continue;
+			}
+			printf("%s:%s\n",location,buffer);
+		}
 		sys_fd_close(fd);
 	}
 	sys_fd_close(data_fd);
