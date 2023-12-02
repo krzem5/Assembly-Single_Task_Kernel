b125179550c7347f9e476aa31669d0d1a944936d
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-12-02 21:49:03 +0100

    Push Update 02/12/2023, 21:49:03

--- src/kernel/socket/socket.c
+++ src/kernel/socket/socket.c
@@ -25,6 +25,7 @@ typedef struct _SOCKET_VFS_NODE{
 	socket_domain_t domain;
 	socket_type_t type;
 	socket_protocol_t protocol;
+	socket_dtp_handler_t* handler;
 	event_t* read_event;
 	event_t* write_event;
 } socket_vfs_node_t;
@@ -44,6 +45,7 @@ static vfs_node_t* _socket_create(void){
 	out->domain=SOCKET_DOMAIN_NONE;
 	out->type=SOCKET_TYPE_NONE;
 	out->protocol=SOCKET_PROTOCOL_NONE;
+	out->handler=NULL;
 	out->read_event=event_new();
 	out->write_event=event_new();
 	return (vfs_node_t*)out;
@@ -91,11 +93,11 @@ void KERNEL_EARLY_EXEC socket_init(void){
 
 KERNEL_PUBLIC void socket_register_dtp_descriptor(const socket_dtp_descriptor_t* descriptor){
 	spinlock_acquire_exclusive(&_socket_dtp_lock);
-	LOG("Registering socket D/T/P handler '%s/%u:%u:%u'...",descriptor->name,descriptor->domain,descriptor->type,descriptor->protocol);
+	LOG("Registering socket D:T:P handler '%s/%u:%u:%u'...",descriptor->name,descriptor->domain,descriptor->type,descriptor->protocol);
 	u32 key=CREATE_DTP_KEY(descriptor->domain,descriptor->type,descriptor->protocol);
 	rb_tree_node_t* node=rb_tree_lookup_node(&_socket_dtp_tree,key);
 	if (node){
-		ERROR("Socket D/T/P %u:%u:%u is already allocated by '%s'",descriptor->domain,descriptor->type,descriptor->protocol,((socket_dtp_handler_t*)node)->descriptor->name);
+		ERROR("Socket D:T:P %u:%u:%u is already allocated by '%s'",descriptor->domain,descriptor->type,descriptor->protocol,((socket_dtp_handler_t*)node)->descriptor->name);
 		spinlock_release_exclusive(&_socket_dtp_lock);
 		return;
 	}
@@ -110,7 +112,7 @@ KERNEL_PUBLIC void socket_register_dtp_descriptor(const socket_dtp_descriptor_t*
 
 KERNEL_PUBLIC void socket_unregister_dtp_descriptor(const socket_dtp_descriptor_t* descriptor){
 	spinlock_acquire_exclusive(&_socket_dtp_lock);
-	LOG("Unregistering socket D/T/P handler '%s/%u:%u:%u'...",descriptor->name,descriptor->domain,descriptor->type,descriptor->protocol);
+	LOG("Unregistering socket D:T:P handler '%s/%u:%u:%u'...",descriptor->name,descriptor->domain,descriptor->type,descriptor->protocol);
 	u32 key=CREATE_DTP_KEY(descriptor->domain,descriptor->type,descriptor->protocol);
 	rb_tree_node_t* node=rb_tree_lookup_node(&_socket_dtp_tree,key);
 	if (node){
@@ -124,9 +126,10 @@ KERNEL_PUBLIC void socket_unregister_dtp_descriptor(const socket_dtp_descriptor_
 
 KERNEL_PUBLIC vfs_node_t* socket_create(vfs_node_t* parent,const string_t* name,socket_domain_t domain,socket_type_t type,socket_protocol_t protocol){
 	spinlock_acquire_shared(&_socket_dtp_lock);
-	rb_tree_node_t* dtp_node=rb_tree_lookup_node(&_socket_dtp_tree,CREATE_DTP_KEY(domain,type,protocol));
+	rb_tree_node_t* handler=rb_tree_lookup_node(&_socket_dtp_tree,CREATE_DTP_KEY(domain,type,protocol));
 	spinlock_release_shared(&_socket_dtp_lock);
-	if (!dtp_node){
+	if (!handler){
+		ERROR("Invalid socket D:T:P combination: %u:%u:%u",domain,type,protocol);
 		return NULL;
 	}
 	vfs_node_t* out=vfs_node_create_virtual(parent,&_socket_vfs_functions,name);
@@ -134,5 +137,6 @@ KERNEL_PUBLIC vfs_node_t* socket_create(vfs_node_t* parent,const string_t* name,
 	((socket_vfs_node_t*)out)->domain=domain;
 	((socket_vfs_node_t*)out)->type=type;
 	((socket_vfs_node_t*)out)->protocol=protocol;
+	((socket_vfs_node_t*)out)->handler=(socket_dtp_handler_t*)handler;
 	return out;
 }
