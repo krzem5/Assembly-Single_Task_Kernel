627ad822a70ff1c1e43f7d94a132bec9faf8cd52
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-12-22 10:29:00 +0100

    Push Update 22/12/2023, 10:29:00

--- src/kernel/linux/syscalls.c
+++ src/kernel/linux/syscalls.c
@@ -319,3 +319,9 @@ void syscall_linux_tee(syscall_reg_state_t* regs){
 void syscall_linux_renameat2(syscall_reg_state_t* regs){
 	panic("syscall_linux_renameat2");
 }
+
+// TODO APIs:
+// better memory api (alloc + counters + locks)
+// drive buffering (internal)
+// ACLs -> acl_get_permissions(handle), acl_set_permissions(handle,clear,set)
+// sockets -> socket_create(domain,type,protocol), socket_create_pair(out[2]), socket_connect(handle,address,address_length), socket_bind(handle,address,address_length), socket_listen(handle,max_connections), socket_accept(handle), socket_getsockname, socket_getpeername, socket_recvfrom, socket_recvmsg, socket_sento, socket_sendmsg, socket_shutdown
