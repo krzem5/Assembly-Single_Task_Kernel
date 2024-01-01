69e328da2f5614a98646cc14702da19c194ee88e
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-01-01 13:36:03 +0100

    Push Update 01/01/2024, 13:36:03

--- src/user/lockinfo/main.c
+++ src/user/lockinfo/main.c
@@ -1,7 +1,9 @@
-#include <sys/clock.h>
-#include <sys/fd.h>
-#include <sys/io.h>
-#include <sys/options.h>
+#include <sys2/clock/clock.h>
+#include <sys2/error/error.h>
+#include <sys2/fd/fd.h>
+#include <sys2/io/io.h>
+#include <sys2/types.h>
+#include <sys2/util/options.h>
 
 
 
@@ -12,42 +14,41 @@ typedef struct _LOCK_TYPE_DATA{
 
 
 
-static _Bool _read_file(s64 fd,const char* name,char* buffer,u32 buffer_length){
+static _Bool _read_file(sys2_fd_t fd,const char* name,char* buffer,u32 buffer_length){
 	if (!buffer_length){
 		return 0;
 	}
-	fd=sys_fd_open(fd,name,SYS_FD_FLAG_READ);
-	if (fd<=0){
+	fd=sys2_fd_open(fd,name,SYS2_FD_FLAG_READ);
+	if (SYS2_IS_ERROR(fd)){
 		return 0;
 	}
-	s64 size=sys_fd_read(fd,buffer,buffer_length-1,0);
-	sys_fd_close(fd);
-	if (size>0){
+	u64 size=sys2_fd_read(fd,buffer,buffer_length-1,0);
+	sys2_fd_close(fd);
+	if (!SYS2_IS_ERROR(size)){
 		buffer[size]=0;
 	}
-	return size>0;
+	return !SYS2_IS_ERROR(size);
 }
 
 
 
 int main(int argc,const char** argv){
-	sys_clock_init();
-	if (!sys_options_parse(argc,argv,NULL)){
+	if (!sys2_options_parse(argc,argv,NULL)){
 		return 1;
 	}
-	s64 root_fd=sys_fd_open(0,"/lock",0);
-	if (!root_fd){
-		printf("lockinfo: unable to open lockfs\n");
+	sys2_fd_t root_fd=sys2_fd_open(0,"/lock",0);
+	if (SYS2_IS_ERROR(root_fd)){
+		sys2_io_print("lockinfo: unable to open lockfs\n");
 		return 1;
 	}
-	s64 types_fd=sys_fd_open(root_fd,"type",0);
+	sys2_fd_t types_fd=sys2_fd_open(root_fd,"type",0);
 	u32 max_type=0;
-	for (s64 iter=sys_fd_iter_start(types_fd);iter>=0;iter=sys_fd_iter_next(iter)){
+	for (sys2_fd_iterator_t iter=sys2_fd_iter_start(types_fd);!SYS2_IS_ERROR(iter);iter=sys2_fd_iter_next(iter)){
 		char name[32];
-		if (sys_fd_iter_get(iter,name,32)<=0){
+		if (SYS2_IS_ERROR(sys2_fd_iter_get(iter,name,32))){
 			continue;
 		}
-		u32 i=sys_options_atoi(name);
+		u32 i=sys2_options_atoi(name);
 		if (i>max_type){
 			max_type=i;
 		}
@@ -57,86 +58,86 @@ int main(int argc,const char** argv){
 		types[i].location[0]=0;
 		types[i].name[0]=0;
 	}
-	for (s64 iter=sys_fd_iter_start(types_fd);iter>=0;iter=sys_fd_iter_next(iter)){
+	for (sys2_fd_iterator_t iter=sys2_fd_iter_start(types_fd);!SYS2_IS_ERROR(iter);iter=sys2_fd_iter_next(iter)){
 		char buffer[32];
-		if (sys_fd_iter_get(iter,buffer,32)<=0){
+		if (SYS2_IS_ERROR(sys2_fd_iter_get(iter,buffer,32))){
 			continue;
 		}
-		u32 i=sys_options_atoi(buffer);
+		u32 i=sys2_options_atoi(buffer);
 		if (!i||i>max_type){
 			continue;
 		}
-		s64 fd=sys_fd_open(types_fd,buffer,0);
-		if (fd<=0){
+		sys2_fd_t fd=sys2_fd_open(types_fd,buffer,0);
+		if (SYS2_IS_ERROR(fd)){
 			continue;
 		}
 		_read_file(fd,"location",(types+i)->location,48);
 		_read_file(fd,"name",(types+i)->name,48);
-		sys_fd_close(fd);
+		sys2_fd_close(fd);
 	}
-	sys_fd_close(types_fd);
-	s64 data_fd=sys_fd_open(root_fd,"data",0);
-	for (s64 iter=sys_fd_iter_start(data_fd);iter>=0;iter=sys_fd_iter_next(iter)){
+	sys2_fd_close(types_fd);
+	sys2_fd_t data_fd=sys2_fd_open(root_fd,"data",0);
+	for (sys2_fd_iterator_t iter=sys2_fd_iter_start(data_fd);!SYS2_IS_ERROR(iter);iter=sys2_fd_iter_next(iter)){
 		char buffer[32];
-		if (sys_fd_iter_get(iter,buffer,32)<=0){
+		if (SYS2_IS_ERROR(sys2_fd_iter_get(iter,buffer,32))){
 			continue;
 		}
-		s64 fd=sys_fd_open(data_fd,buffer,0);
-		if (fd<=0){
+		sys2_fd_t fd=sys2_fd_open(data_fd,buffer,0);
+		if (SYS2_IS_ERROR(fd)){
 			continue;
 		}
 		char location[48];
 		if (!_read_file(fd,"location",location,48)){
-			sys_fd_close(fd);
+			sys2_fd_close(fd);
 			continue;
 		}
 		char name[48];
 		if (!_read_file(fd,"name",name,48)){
-			sys_fd_close(fd);
+			sys2_fd_close(fd);
 			continue;
 		}
 		_Bool header_printed=0;
-		for (s64 iter=sys_fd_iter_start(fd);iter>=0;iter=sys_fd_iter_next(iter)){
-			if (sys_fd_iter_get(iter,buffer,32)<=0||buffer[0]<48||buffer[0]>57){
+		for (sys2_fd_iterator_t iter_inner=sys2_fd_iter_start(fd);!SYS2_IS_ERROR(iter_inner);iter_inner=sys2_fd_iter_next(iter_inner)){
+			if (SYS2_IS_ERROR(sys2_fd_iter_get(iter_inner,buffer,32))||buffer[0]<48||buffer[0]>57){
 				continue;
 			}
-			u32 type=sys_options_atoi(buffer);
+			u32 type=sys2_options_atoi(buffer);
 			if (type>max_type){
 				continue;
 			}
-			s64 subfd=sys_fd_open(fd,buffer,0);
-			if (subfd<=0){
+			sys2_fd_t subfd=sys2_fd_open(fd,buffer,0);
+			if (SYS2_IS_ERROR(subfd)){
 				continue;
 			}
 			if (!_read_file(subfd,"count",buffer,32)){
 				goto _cleanup_subfd;
 			}
-			u32 count=sys_options_atoi(buffer);
+			u32 count=sys2_options_atoi(buffer);
 			if (!count||!_read_file(subfd,"ticks",buffer,32)){
 				goto _cleanup_subfd;
 			}
-			u32 ticks=sys_options_atoi(buffer);
+			u32 ticks=sys2_options_atoi(buffer);
 			if (!_read_file(subfd,"max_ticks",buffer,32)){
 				goto _cleanup_subfd;
 			}
-			u32 max_ticks=sys_options_atoi(buffer);
+			u32 max_ticks=sys2_options_atoi(buffer);
 			if (!header_printed){
-				printf("%s \x1b[2;3m%s\x1b[0m\n",location,name);
+				sys2_io_print("%s \x1b[2;3m%s\x1b[0m\n",location,name);
 				header_printed=1;
 			}
 			if (type){
-				printf("    %s \x1b[2;3m%s\x1b[0m\n",(types+type)->location,(types+type)->name);
+				sys2_io_print("    %s \x1b[2;3m%s\x1b[0m\n",(types+type)->location,(types+type)->name);
 			}
 			else{
-				printf("    %s\n",(types+type)->location);
+				sys2_io_print("    %s\n",(types+type)->location);
 			}
-			printf("        cnt: \x1b[1m%lu\x1b[0m\n        avg: \x1b[1m%lu\x1b[0m ns\n        max: \x1b[1m%lu\x1b[0m ns\n",count,sys_clock_ticks_to_time(ticks/count),sys_clock_ticks_to_time(max_ticks));
+			sys2_io_print("        cnt: \x1b[1m%lu\x1b[0m\n        avg: \x1b[1m%lu\x1b[0m ns\n        max: \x1b[1m%lu\x1b[0m ns\n",count,sys2_clock_convert_ticks_to_time(ticks/count),sys2_clock_convert_ticks_to_time(max_ticks));
 _cleanup_subfd:
-			sys_fd_close(subfd);
+			sys2_fd_close(subfd);
 		}
-		sys_fd_close(fd);
+		sys2_fd_close(fd);
 	}
-	sys_fd_close(data_fd);
-	sys_fd_close(root_fd);
+	sys2_fd_close(data_fd);
+	sys2_fd_close(root_fd);
 	return 0;
 }
