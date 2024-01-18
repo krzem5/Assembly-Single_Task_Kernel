673138588b8eb77f558b734a0d1c93bf82e869b8
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-01-18 17:42:47 +0100

    Push Update 18/01/2024, 17:42:47

--- src/module/virtio_fs/fs.c
+++ src/module/virtio_fs/fs.c
@@ -1,5 +1,6 @@
 #include <kernel/lock/spinlock.h>
 #include <kernel/log/log.h>
+#include <kernel/memory/amm.h>
 #include <kernel/memory/omm.h>
 #include <kernel/memory/pmm.h>
 #include <kernel/memory/vmm.h>
@@ -19,13 +20,45 @@ static omm_allocator_t* _virtio_fs_device_allocator=NULL;
 
 
 static _Bool _virtio_driver_init(virtio_device_t* device,u64 features){
-	// LOG("AAAA");
 	virtio_fs_config_t config;
 	for (u32 i=0;i<sizeof(config.raw_data)/sizeof(u32);i++){
 		config.raw_data[i]=virtio_read(device->device_field+i*sizeof(u32),4);
 	}
-	WARN("%s %u",config.tag,config.num_request_queues);
+	if (!config.tag[0]||!config.num_request_queues){
+		return 0;
+	}
+	virtio_queue_t* hiprioq=virtio_init_queue(device,0);
+	if (!hiprioq){
+		return 0;
+	}
+	virtio_queue_t* loprioq=virtio_init_queue(device,1);
+	if (!loprioq){
+		return 0;
+	}
+	config.tag[35]=0;
+	INFO("Creating File System device...");
+	INFO("Filesystem name: %s",config.tag);
 	// panic("A");
+// 	virtio_fs_control_header_t* request=amm_alloc(sizeof(virtio_fs_control_header_t));
+// 	request->type=VIRTIO_GPU_CMD_GET_DISPLAY_INFO;
+// 	request->flags=VIRTIO_GPU_FLAG_FENCE;
+// 	request->fence_id=0;
+// 	virtio_fs_resp_display_info_t* response=amm_alloc(sizeof(virtio_fs_resp_display_info_t));
+// 	virtio_buffer_t buffers[2]={
+// 		{
+// 			vmm_virtual_to_physical(&vmm_kernel_pagemap,(u64)request),
+// 			sizeof(virtio_fs_control_header_t)
+// 		},
+// 		{
+// 			vmm_virtual_to_physical(&vmm_kernel_pagemap,(u64)response),
+// 			sizeof(virtio_fs_resp_display_info_t)
+// 		}
+// 	};
+// 	virtio_queue_transfer(fs_device->controlq,buffers,1,1);
+// 	virtio_queue_wait(fs_device->controlq);
+// 	virtio_queue_pop(fs_device->controlq,NULL);
+// 	amm_dealloc(request);
+// 	amm_dealloc(responce);
 	return 1;
 }
 
--- src/module/virtio_fs/include/virtio/fs_registers.h
+++ src/module/virtio_fs/include/virtio/fs_registers.h
@@ -4,6 +4,10 @@
 
 
 
+#define FUSE_SUBMOUNTS 0x08000000
+
+
+
 typedef struct KERNEL_PACKED _VIRTIO_FS_CONFIG{
 	union KERNEL_PACKED{
 		u32 raw_data[11];
@@ -14,6 +18,34 @@ typedef struct KERNEL_PACKED _VIRTIO_FS_CONFIG{
 		};
 	};
 } virtio_fs_config_t;
+
+
+
+typedef struct _FUSE_INIT_IN{
+	u32 major;
+	u32 minor;
+	u32 max_readahead;
+	u32 flags;
+	u32 flags2;
+	u32 unused[11];
+} fuse_init_in_t;
+
+
+
+typedef struct _FUSE_INIT_OUT{
+	u32 major;
+	u32 minor;
+	u32 max_readahead;
+	u32 flags;
+	u16 max_background;
+	u16 congestion_threshold;
+	u32 max_write;
+	u32 time_gran;
+	u16 max_pages;
+	u16 map_alignment;
+	u32 flags2;
+	u32 unused[7];
+} fuse_init_out;
 
 
 
