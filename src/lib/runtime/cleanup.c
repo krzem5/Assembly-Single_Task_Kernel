492ed11eda485cb10464c6181d9368ee1e930b42
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-02-16 18:51:37 +0100

    Push Update 16/02/2024, 18:51:37

--- src/lib/runtime/cleanup.c
+++ src/lib/runtime/cleanup.c
@@ -5,14 +5,14 @@
 
 extern void __sys_fini(void) __attribute__((weak));
 extern void __sys_linker_set_root_object_gcov_info(u64,u64) __attribute__((weak));
-extern u64 __start_gcov_info[1];
-extern u64 __stop_gcov_info[1];
+extern u64 __gcov_info_start[1];
+extern u64 __gcov_info_end[1];
 
 
 
 void _execute_fini(void){
 #if KERNEL_COVERAGE_ENABLED
-	__sys_linker_set_root_object_gcov_info((u64)__start_gcov_info,((u64)__stop_gcov_info)-((u64)__start_gcov_info));
+	__sys_linker_set_root_object_gcov_info((u64)__gcov_info_start,((u64)__gcov_info_end)-((u64)__gcov_info_start));
 #endif
 	__sys_fini();
 }
