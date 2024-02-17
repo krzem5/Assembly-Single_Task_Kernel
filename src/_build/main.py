8a203372cd42200fff18801cb0b9bc1f54d18770
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-02-17 11:41:21 +0100

    Push Update 17/02/2024, 11:41:21

--- src/_build/main.py
+++ src/_build/main.py
@@ -775,7 +775,7 @@ if (rebuild_data_partition):
 #####################################################################################################################################
 if ("--run" in sys.argv):
 	if (not NO_FILE_SERVER):
-		subprocess.Popen([("/usr/libexec/virtiofsd" if not os.getenv("GITHUB_ACTIONS","") else "build/tmp/virtiofsd/target/release/virtiofsd"),f"--socket-group={(os.getlogin() if not os.getenv('GITHUB_ACTIONS','') else 'docker')}","--socket-path=build/virtiofsd.sock","--shared-dir","build/share","--inode-file-handles=mandatory"])
+		subprocess.Popen((["/usr/libexec/virtiofsd",f"--socket-group={os.getlogin()}"] if not os.getenv("GITHUB_ACTIONS","") else ["sudo","build/tmp/virtiofsd/target/release/virtiofsd"])+["--socket-path=build/virtiofsd.sock","--shared-dir","build/share","--inode-file-handles=mandatory"])
 	if (not os.path.exists("build/vm/hdd.qcow2")):
 		if (subprocess.run(["qemu-img","create","-q","-f","qcow2","build/vm/hdd.qcow2","16G"]).returncode!=0):
 			sys.exit(1)
@@ -788,7 +788,7 @@ if ("--run" in sys.argv):
 	if (not os.path.exists("build/vm/OVMF_VARS.fd")):
 		if (subprocess.run(["cp","/usr/share/OVMF/OVMF_VARS.fd","build/vm/OVMF_VARS.fd"]).returncode!=0):
 			sys.exit(1)
-	subprocess.run([
+	subprocess.run(([] if not os.getenv("GITHUB_ACTIONS","") else ["sudo"])+[
 		"qemu-system-x86_64",
 		# "-d","trace:virtio*,trace:virtio_blk*",
 		# "-d","trace:virtio*,trace:virtio_gpu*",
