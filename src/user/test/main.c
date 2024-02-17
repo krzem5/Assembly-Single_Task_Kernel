4110b905b97ae0d5ce2ec86593a95c1ca799fec4
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-02-17 10:14:58 +0100

    Push Update 17/02/2024, 10:14:58

--- src/user/test/main.c
+++ src/user/test/main.c
@@ -7,6 +7,6 @@
 
 
 void main(void){
-	// sys_thread_await_event(sys_process_get_termination_event(sys_process_start("/bin/ls",0,NULL,NULL,0)));
+	sys_thread_await_event(sys_process_get_termination_event(sys_process_start("/bin/ls",0,NULL,NULL,0)));
 	sys_system_shutdown(0);
 }
