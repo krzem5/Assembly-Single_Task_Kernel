829fa3bf277923a2dcf45a72a29c3c32fe8e24e2
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-02-18 12:48:18 +0100

    Push Update 18/02/2024, 12:48:18

--- src/module/test/test/crash.c
+++ src/module/test/test/crash.c
@@ -4,6 +4,8 @@
 #include <kernel/log/log.h>
 #include <kernel/mp/event.h>
 #include <kernel/mp/process.h>
+#include <kernel/mp/thread.h>
+#include <kernel/scheduler/scheduler.h>
 #include <kernel/syscall/syscall.h>
 #include <kernel/types.h>
 #include <kernel/util/util.h>
@@ -42,21 +44,62 @@ static void _execute_elf(const char* path){
 
 
 
+static void _thread_no_crash(void){
+	_test_crash_state=0x01;
+}
+
+
+
+static void _thread_crash_invalid_read(void){
+	(void)(*((volatile const u8*)NULL));
+	_test_crash_state=0x01;
+}
+
+
+
+static void _thread_crash_invalid_write(void){
+	*((volatile u8*)NULL)=0x00;
+	_test_crash_state=0x01;
+}
+
+
+
 void test_crash(void){
 	TEST_MODULE("crash");
 	syscall_create_table("test_crash",_test_crash_syscall_functions,sizeof(_test_crash_syscall_functions)/sizeof(syscall_callback_t));
 	TEST_FUNC("_isr_handler");
-	TEST_GROUP("no crash");
+	TEST_GROUP("no user crash");
 	_test_crash_state=0x00;
 	_execute_elf("/bin/test_crash_normal");
 	TEST_ASSERT(_test_crash_state==0x01);
+	TEST_GROUP("invalid user read");
 	_test_crash_state=0x00;
 	_execute_elf("/bin/test_crash_invalid_read");
 	TEST_ASSERT(_test_crash_state==0x00);
+	TEST_GROUP("invalid user write");
 	_test_crash_state=0x00;
 	_execute_elf("/bin/test_crash_invalid_write");
 	TEST_ASSERT(_test_crash_state==0x00);
+	TEST_GROUP("invalid user instruction");
 	_test_crash_state=0x00;
 	_execute_elf("/bin/test_crash_wrong_iopl");
+	TEST_ASSERT(_test_crash_state==0x00);
+	TEST_GROUP("no kernel crash");
+	_test_crash_state=0x00;
+	process_t* process=process_create("test-process","test-process");
+	scheduler_enqueue_thread(thread_create_kernel_thread(process,"test-crash-kernel-no-crash",_thread_no_crash,0x200000,0));
+	event_await(process->event,0);
+	TEST_ASSERT(_test_crash_state==0x01);
+	TEST_GROUP("invalid kernel read");
+	_test_crash_state=0x00;
+	process=process_create("test-process","test-process");
+	scheduler_enqueue_thread(thread_create_kernel_thread(process,"test-crash-kernel-invalid-read",_thread_crash_invalid_read,0x200000,0));
+	event_await(process->event,0);
+	TEST_ASSERT(_test_crash_state==0x00);
+	TEST_GROUP("invalid kernel write");
+	_test_crash_state=0x00;
+	process=process_create("test-process","test-process");
+	scheduler_enqueue_thread(thread_create_kernel_thread(process,"test-crash-kernel-invalid-write",_thread_crash_invalid_write,0x200000,0));
+	event_await(process->event,0);
 	TEST_ASSERT(_test_crash_state==0x00);
 }
