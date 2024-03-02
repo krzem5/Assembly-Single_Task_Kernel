5f1eb2e7823a0143571a6d879a2427b3acb6d31c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-03-02 19:25:11 +0100

    Push Update 02/03/2024, 19:25:11

--- src/kernel/aslr/aslr.c
+++ src/kernel/aslr/aslr.c
@@ -10,8 +10,8 @@
 
 
 
-#define KERNEL_ASLR_KERNEL_END (kernel_get_offset()+0x8000000)
-#define KERNEL_ASLR_MODULE_START (kernel_get_offset()+0xc000000)
+#define KERNEL_ASLR_KERNEL_END 0xffffffffc8000000ull
+#define KERNEL_ASLR_MODULE_START 0xffffffffcc000000ull
 
 
 
@@ -50,19 +50,20 @@ u64 aslr_generate_address(u64 min,u64 max){
 void KERNEL_EARLY_EXEC KERNEL_NORETURN aslr_reloc_kernel(void (*KERNEL_NORETURN next_stage_callback)(void)){
 	aslr_module_base=aslr_generate_address(KERNEL_ASLR_KERNEL_END,KERNEL_ASLR_MODULE_START);
 	aslr_module_size=-PAGE_SIZE-KERNEL_ASLR_MODULE_START;
-#ifdef KERNEL_RELEASE
+#ifndef KERNEL_RELEASE
 	ERROR("ASLR disabled");
 	(void)_finish_relocation;
 	INFO("Kernel range: %p - %p",kernel_section_kernel_start(),kernel_section_kernel_end());
 	INFO("Module range: %p - %p",aslr_module_base,aslr_module_base+aslr_module_size);
 	next_stage_callback();
 #else
-	LOG("Relocating kernel...");
+	LOG("Generating ASLR addresses...");
 	u64 kernel_size=pmm_align_up_address(kernel_section_kernel_end()-kernel_section_kernel_start());
 	random_generate(&_aslr_offset,sizeof(u64));
 	_aslr_offset=pmm_align_down_address(_aslr_offset%(KERNEL_ASLR_KERNEL_END-kernel_size-kernel_section_kernel_end()-1));
 	INFO("Kernel range: %p - %p",kernel_section_kernel_end()+_aslr_offset,kernel_section_kernel_end()+_aslr_offset+kernel_size);
 	INFO("Module range: %p - %p",aslr_module_base,aslr_module_base+aslr_module_size);
+	LOG("Relocating kernel...");
 	for (u64 i=kernel_section_kernel_start();i<kernel_section_kernel_end();i+=PAGE_SIZE){
 		vmm_map_page(&vmm_kernel_pagemap,i-kernel_get_offset(),i+_aslr_offset,VMM_PAGE_FLAG_READWRITE|VMM_PAGE_FLAG_PRESENT);
 	}
