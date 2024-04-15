6a75ffd7aeea76fc19d09bdb3682c09dc5165510
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-04-15 09:53:36 +0200

    Push Update 15/04/2024, 09:53:36

--- src/module/keyringstore/store.c
+++ src/module/keyringstore/store.c
@@ -1,4 +1,5 @@
 #include <kernel/config/config.h>
+#include <kernel/format/format.h>
 #include <kernel/keyring/keyring.h>
 #include <kernel/log/log.h>
 #include <kernel/memory/smm.h>
@@ -41,14 +42,17 @@ static vfs_node_t* _get_store_directory(void){
 
 
 static void _store_keyring(keyring_t* keyring){
+	INFO("Storing keyring '%s'...",keyring->name->data);
 	config_tag_t* root_tag=config_tag_create(CONFIG_TAG_TYPE_ARRAY,"");
 	config_tag_t* name_tag=config_tag_create(CONFIG_TAG_TYPE_STRING,"name");
 	smm_dealloc(name_tag->string);
 	name_tag->string=smm_duplicate(keyring->name);
 	config_tag_attach(root_tag,name_tag);
+	char buffer[32];
+	format_string(buffer,32,"%lu.config",HANDLE_ID_GET_INDEX(keyring->handle.rb_node.key));
 	vfs_node_t* parent;
 	const char* child_name;
-	vfs_node_t* node=vfs_lookup_for_creation(_keyringstore_root_dir,keyring->name->data,0,0,0,&parent,&child_name);
+	vfs_node_t* node=vfs_lookup_for_creation(_keyringstore_root_dir,buffer,0,0,0,&parent,&child_name);
 	if (!node){
 		SMM_TEMPORARY_STRING child_name_string=smm_alloc(child_name,0);
 		node=vfs_node_create(NULL,_keyringstore_root_dir,child_name_string,VFS_NODE_TYPE_FILE|VFS_NODE_FLAG_CREATE);
@@ -61,7 +65,6 @@ static void _store_keyring(keyring_t* keyring){
 	node->flags|=(0400<<VFS_NODE_PERMISSION_SHIFT)|VFS_NODE_FLAG_DIRTY;
 	vfs_node_flush(node);
 	config_save_to_file(root_tag,node,NULL);
-	WARN("Store keyring: %s",keyring->name->data);
 	config_tag_delete(root_tag);
 }
 
