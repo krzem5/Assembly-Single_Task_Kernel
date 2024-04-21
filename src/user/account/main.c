74de10f02f53d8ad10a131705084834a909d13ed
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-04-21 20:18:39 +0200

    Push Update 21/04/2024, 20:18:39

--- src/user/account/main.c
+++ src/user/account/main.c
@@ -16,7 +16,13 @@ int main(int argc,const char** argv){
 		if (SYS_IS_ERROR(account_get_user_data(uid,&data))||SYS_IS_ERROR(sys_uid_get_name(uid,buffer,sizeof(buffer)))){
 			continue;
 		}
-		sys_io_print("\x1b[1m%u\x1b[0m:\t%s\t%s\n",uid,buffer,((data.flags&ACCOUNT_USER_FLAG_HAS_PASSWORD)?"(has password)":""));
+		sys_io_print("\x1b[1m%u(%s)\x1b[0m:\t%s\n",uid,buffer,((data.flags&ACCOUNT_USER_FLAG_HAS_PASSWORD)?"(has password)":""));
+		for (sys_gid_t gid=account_iter_user_group_start(uid);gid;gid=account_iter_user_group_next(uid,gid)){
+			if (SYS_IS_ERROR(sys_gid_get_name(gid,buffer,sizeof(buffer)))){
+				continue;
+			}
+			sys_io_print("  \x1b[1m%u(%s)\x1b[0m\n",gid,buffer);
+		}
 	}
 	sys_io_print("Groups:\n");
 	for (sys_gid_t gid=account_iter_group_start();gid;gid=account_iter_group_next(gid)){
@@ -25,7 +31,7 @@ int main(int argc,const char** argv){
 		if (SYS_IS_ERROR(account_get_group_data(gid,&data))||SYS_IS_ERROR(sys_gid_get_name(gid,buffer,sizeof(buffer)))){
 			continue;
 		}
-		sys_io_print("\x1b[1m%u\x1b[0m:\t%s\n",gid,buffer);
+		sys_io_print("\x1b[1m%u(%s)\x1b[0m\n",gid,buffer);
 	}
 	return 0;
 }
