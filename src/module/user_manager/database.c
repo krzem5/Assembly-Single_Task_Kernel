c83a67297cf757a7df5b6cd6120d8d95c2cfa44f
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-04-21 13:16:53 +0200

    Push Update 21/04/2024, 13:16:53

--- src/module/user_manager/database.c
+++ src/module/user_manager/database.c
@@ -1,13 +1,16 @@
 #include <kernel/config/config.h>
 #include <kernel/error/error.h>
+#include <kernel/hash/sha256.h>
 #include <kernel/id/user.h>
 #include <kernel/lock/spinlock.h>
 #include <kernel/log/log.h>
 #include <kernel/memory/omm.h>
 #include <kernel/memory/pmm.h>
+#include <kernel/memory/smm.h>
 #include <kernel/module/module.h>
 #include <kernel/tree/rb_tree.h>
 #include <kernel/types.h>
+#include <kernel/util/memory.h>
 #include <kernel/vfs/node.h>
 #include <kernel/vfs/vfs.h>
 #define KERNEL_LOG_NAME "user_database"
@@ -19,11 +22,13 @@
 
 #define USER_DATABASE_FIRST_USER_ID 1000
 
+#define USER_DATABASE_ENTRY_FLAG_HAS_PASSWORD 1
+
 
 
 typedef struct _USER_DATABASE_ENTRY{
 	rb_tree_node_t rb_node;
-	u32 has_password;
+	u32 flags;
 	u32 password_hash[8];
 } user_database_entry_t;
 
@@ -41,7 +46,37 @@ static user_database_t _user_database;
 
 
 
+static config_tag_t* _generate_database_config(void){
+	config_tag_t* root_tag=config_tag_create(CONFIG_TAG_TYPE_ARRAY,"");
+	for (const user_database_entry_t* entry=(void*)rb_tree_iter_start(&(_user_database.tree));entry;entry=(void*)rb_tree_iter_next(&(_user_database.tree),(void*)entry)){
+		config_tag_t* user_tag=config_tag_create(CONFIG_TAG_TYPE_ARRAY,"user");
+		config_tag_attach(root_tag,user_tag);
+		config_tag_t* id_tag=config_tag_create(CONFIG_TAG_TYPE_INT,"id");
+		config_tag_attach(user_tag,id_tag);
+		id_tag->int_=entry->rb_node.key;
+		config_tag_t* flags_tag=config_tag_create(CONFIG_TAG_TYPE_INT,"flags");
+		config_tag_attach(user_tag,flags_tag);
+		flags_tag->int_=entry->flags;
+		config_tag_t* name_tag=config_tag_create(CONFIG_TAG_TYPE_STRING,"name");
+		config_tag_attach(user_tag,name_tag);
+		smm_dealloc(name_tag->string);
+		char buffer[256];
+		uid_get_name(entry->rb_node.key,buffer,sizeof(buffer));
+		name_tag->string=smm_alloc(buffer,0);
+		if (entry->flags&USER_DATABASE_ENTRY_FLAG_HAS_PASSWORD){
+			config_tag_t* password_hash_tag=config_tag_create(CONFIG_TAG_TYPE_STRING,"password_hash");
+			config_tag_attach(user_tag,password_hash_tag);
+			smm_dealloc(password_hash_tag->string);
+			password_hash_tag->string=smm_alloc((void*)(entry->password_hash),8*sizeof(u32));
+		}
+	}
+	return root_tag;
+}
+
+
+
 static void _load_user_database(void){
+	LOG("Loading user database...");
 	vfs_node_t* node=vfs_lookup(NULL,USER_DATABASE_FILE,0,0,0);
 	if (!node){
 		return;
@@ -56,6 +91,31 @@ static void _load_user_database(void){
 		return;
 	}
 	ERROR("_load_user_database: Unimplemented");
+	config_tag_delete(root_tag);
+}
+
+
+
+static void _save_user_database(void){
+	LOG("Saving user database...");
+	config_tag_t* root_tag=_generate_database_config();
+	vfs_node_t* parent;
+	const char* child_name;
+	vfs_node_t* node=vfs_lookup_for_creation(NULL,USER_DATABASE_FILE,0,0,0,&parent,&child_name);
+	if (!node){
+		SMM_TEMPORARY_STRING child_name_string=smm_alloc(child_name,0);
+		node=vfs_node_create(NULL,parent,child_name_string,VFS_NODE_TYPE_FILE|VFS_NODE_FLAG_CREATE);
+		if (!node){
+			ERROR("Unable to create user database file");
+			return;
+		}
+	}
+	node->uid=0;
+	node->gid=0;
+	node->flags&=~VFS_NODE_PERMISSION_MASK;
+	node->flags|=(0400<<VFS_NODE_PERMISSION_SHIFT)|VFS_NODE_FLAG_DIRTY;
+	vfs_node_flush(node);
+	config_save_to_file(root_tag,node,USER_DATABASE_PASSWORD,0);
 	config_tag_delete(root_tag);
 }
 
@@ -88,7 +148,17 @@ _regenerate_uid:
 	}
 	user_database_entry_t* entry=omm_alloc(_user_database_entry_allocator);
 	entry->rb_node.key=uid;
-	entry->has_password=0;
+	entry->flags=0;
+	if (password_length){
+		hash_sha256_state_t state;
+		hash_sha256_init(&state);
+		hash_sha256_process_chunk(&state,password,password_length);
+		hash_sha256_finalize(&state);
+		entry->flags|=USER_DATABASE_ENTRY_FLAG_HAS_PASSWORD;
+		mem_copy(entry->password_hash,state.result,8*sizeof(u32));
+	}
+	rb_tree_insert_node(&(_user_database.tree),&(entry->rb_node));
+	_save_user_database();
 	spinlock_release_exclusive(&(_user_database.lock));
 	return uid;
 }
@@ -104,7 +174,6 @@ MODULE_INIT(){
 
 
 MODULE_POSTINIT(){
-	LOG("Loading user database...");
 	_load_user_database();
-	WARN("UID=%u",user_database_create_user(0,"user",NULL,0));
+	// WARN("UID=%u",user_database_create_user(0,"user","abc",3));
 }
