f52cbb31896b2f57d131a75feba9fd5fe9d465c6
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-06-03 22:04:52 +0200

    Push Update 03/06/2024, 22:04:52

--- src/_build/main.py
+++ src/_build/main.py
@@ -812,9 +812,7 @@ def _execute_vm():
 		# Shared directory
 		*(["-chardev","socket,id=virtio-fs-sock,path=build/vm/virtiofsd.sock","-device","vhost-user-fs-pci,queue-size=1024,chardev=virtio-fs-sock,tag=build-fs"] if not NO_FILE_SERVER else []),
 		# Serial
-		"-chardev","stdio,id=console,mux=on",
-		"-mon","chardev=console,mode=readline",
-		"-serial","chardev:console",
+		"-serial","mon:stdio",
 		"-serial",("file:build/raw_coverage" if mode==MODE_COVERAGE else "null"),
 		# Config
 		"-machine","q35,hmat=on",
