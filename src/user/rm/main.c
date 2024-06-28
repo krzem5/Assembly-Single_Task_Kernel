4653f0c1748d5817c0a67abd41da8c800f6d08a9
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-06-28 18:41:28 +0200

    Push Update 28/06/2024, 18:41:28

--- src/user/rm/main.c
+++ src/user/rm/main.c
@@ -13,7 +13,7 @@ int main(int argc,const char** argv){
 	}
 	sys_fd_t fd=sys_fd_open(0,argv[i],0);
 	if (SYS_IS_ERROR(fd)){
-		sys_io_print("touch: unable to open file '%s': error %d\n",argv[i],fd);
+		sys_io_print("rm: unable to open file '%s': error %d\n",argv[i],fd);
 		return 1;
 	}
 	sys_fd_unlink(fd);
