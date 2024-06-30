c282fd5df4f474369c55b601ff12f7fd644ef1bc
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-06-30 22:36:43 +0200

    Push Update 30/06/2024, 22:36:43

--- src/module/net_ip/include/net/ip6.h
+++ src/module/net_ip/include/net/ip6.h
@@ -4,10 +4,69 @@
 
 
 
+typedef u8 net_ip6_protocol_type_t;
+
+
+
 typedef struct _NET_IP6_ADDRESS{
 	u64 prefix;
 	u64 identifier;
 } net_ip6_address_t;
+
+
+
+typedef struct KERNEL_PACKED _NET_IP6_PACKET_DATA{
+	u8 version_traffic_class_flow_label[4];
+	u16 payload_length;
+	u8 next_header;
+	u8 hop_limit;
+	u64 src_address[2];
+	u64 dst_address[2];
+	u8 data[];
+} net_ip6_packet_data_t;
+
+
+
+typedef struct _NET_IP6_PACKET{
+	u16 length;
+	network_layer1_packet_t* raw_packet;
+	net_ip6_packet_data_t* packet;
+} net_ip6_packet_t;
+
+
+
+typedef struct _NET_IP6_PROTOCOL_DESCRIPTOR{
+	const char* name;
+	net_ip6_protocol_type_t protocol_type;
+	void (*rx_callback)(net_ip6_packet_t* packet);
+} net_ip6_protocol_descriptor_t;
+
+
+
+typedef struct _NET_IP6_PROTOCOL{
+	rb_tree_node_t rb_node;
+	const net_ip6_protocol_descriptor_t* descriptor;
+} net_ip6_protocol_t;
+
+
+
+void net_ip6_register_protocol_descriptor(const net_ip6_protocol_descriptor_t* descriptor);
+
+
+
+void net_ip6_unregister_protocol_descriptor(const net_ip6_protocol_descriptor_t* descriptor);
+
+
+
+net_ip6_packet_t* net_ip6_create_packet(u16 length,const net_ip6_address_t* src_address,const net_ip6_address_t* dst_address,net_ip6_protocol_type_t protocol_type);
+
+
+
+void net_ip6_delete_packet(net_ip6_packet_t* packet);
+
+
+
+void net_ip6_send_packet(net_ip6_packet_t* packet);
 
 
 
--- src/module/net_ip/ip6.c
+++ src/module/net_ip/ip6.c
@@ -1,13 +1,25 @@
+#include <kernel/lock/rwlock.h>
 #include <kernel/log/log.h>
+#include <kernel/memory/omm.h>
 #include <kernel/module/module.h>
 #include <kernel/network/layer1.h>
 #include <kernel/network/layer2.h>
+#include <kernel/tree/rb_tree.h>
 #include <kernel/types.h>
+#include <kernel/util/util.h>
+#include <net/ip6.h>
 #define KERNEL_LOG_NAME "net_ip6"
 
 
 
 #define ETHER_TYPE 0x86dd
+
+
+
+static rwlock_t _net_ip6_protocol_lock;
+static rb_tree_t _net_ip6_protocol_type_tree;
+static omm_allocator_t* KERNEL_INIT_WRITE _net_ip6_protocol_allocator=NULL;
+static omm_allocator_t* KERNEL_INIT_WRITE _net_ip6_packet_allocator=NULL;
 
 
 
@@ -25,7 +37,48 @@ static const network_layer2_protocol_descriptor_t _net_ip6_protocol_descriptor={
 
 
 
+MODULE_INIT(){
+	rwlock_init(&_net_ip6_protocol_lock);
+	rb_tree_init(&_net_ip6_protocol_type_tree);
+	_net_ip6_protocol_allocator=omm_init("net.ip6.protocol",sizeof(net_ip6_protocol_t),8,1);
+	rwlock_init(&(_net_ip6_protocol_allocator->lock));
+	_net_ip6_packet_allocator=omm_init("net.ip6.packet",sizeof(net_ip6_packet_t),8,4);
+	rwlock_init(&(_net_ip6_packet_allocator->lock));
+}
+
+
+
 MODULE_POSTINIT(){
 	LOG("Registering IPv6 protocol...");
 	network_layer2_register_descriptor(&_net_ip6_protocol_descriptor);
+}
+
+
+
+KERNEL_PUBLIC void net_ip6_register_protocol_descriptor(const net_ip6_protocol_descriptor_t* descriptor){
+	panic("net_ip6_register_protocol_descriptor");
+}
+
+
+
+KERNEL_PUBLIC void net_ip6_unregister_protocol_descriptor(const net_ip6_protocol_descriptor_t* descriptor){
+	panic("net_ip6_unregister_protocol_descriptor");
+}
+
+
+
+KERNEL_PUBLIC net_ip6_packet_t* net_ip6_create_packet(u16 length,const net_ip6_address_t* src_address,const net_ip6_address_t* dst_address,net_ip6_protocol_type_t protocol_type){
+	panic("net_ip6_create_packet");
+}
+
+
+
+KERNEL_PUBLIC void net_ip6_delete_packet(net_ip6_packet_t* packet){
+	panic("net_ip6_delete_packet");
+}
+
+
+
+KERNEL_PUBLIC void net_ip6_send_packet(net_ip6_packet_t* packet){
+	panic("net_ip6_send_packet");
 }
