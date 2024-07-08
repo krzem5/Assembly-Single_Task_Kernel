d2f54f6a724804538272608168c7795c3278cf66
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-07-08 13:32:12 +0200

    Push Update 08/07/2024, 13:32:12

--- src/_build/main.py
+++ src/_build/main.py
@@ -421,7 +421,7 @@ def _execute_vm():
 
 
 
-empty_directories=option("build_directories.empty").data[:]
+empty_directories=option("build_directories.empty").data
 if (os.path.exists("build/last_mode")):
 	with open("build/last_mode","r") as rf:
 		if (int(rf.read())!=mode):
