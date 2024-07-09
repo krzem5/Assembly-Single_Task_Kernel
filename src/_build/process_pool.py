c48230c70c0a8f4fc605d892fc739aeda3386b33
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-07-09 10:18:40 +0200

    Push Update 09/07/2024, 10:18:40

--- src/_build/process_pool.py
+++ src/_build/process_pool.py
@@ -24,6 +24,9 @@ class ProcessPoolCommand(object):
 		if (not self.dependencies):
 			self._trigger()
 
+	def fail(self):
+		self.pool.fail(self.file)
+
 	def _trigger(self):
 		thr=threading.Thread(target=self._thread)
 		thr.start()
@@ -46,6 +49,7 @@ class ProcessPoolCommand(object):
 			elif (self.file.startswith("build") and os.path.exists(self.file)):
 				os.remove(self.file)
 			self.pool._error=True
+			self.pool.fail(self.file)
 		else:
 			self.pool.dispatch(self.file)
 		self.pool._process_count-=1
@@ -63,23 +67,43 @@ class ProcessPool(object):
 
 	def add(self,dependencies,file,name,command):
 		pool_command=ProcessPoolCommand(self,file,dependencies,command,name)
+		fail=False
 		self._lock.acquire()
 		for dep in dependencies:
 			dep_state=self._dependency_map.get(dep,[])
 			if (dep_state is True):
 				pool_command.drop_dependency(dep)
+			elif (dep_state is False):
+				fail=True
+				break
 			else:
 				dep_state.append(pool_command)
 				self._dependency_map[dep]=dep_state
 		self._process_count+=1
 		self._lock.release()
+		if (fail):
+			self.fail(file)
+
+	def fail(self,file):
+		self._lock.acquire()
+		fail_list=[file]
+		while (fail_list):
+			file=fail_list.pop()
+			if (file in self._dependency_map and self._dependency_map[file] is not False):
+				for pool_command in self._dependency_map[file]:
+					fail_list.append(pool_command.file)
+			self._dependency_map[file]=False
+		self._lock.release()
 
 	def dispatch(self,file):
 		self._lock.acquire()
-		if (file in self._dependency_map and self._dependency_map[file] is not True):
-			for pool_command in self._dependency_map[file]:
-				pool_command.drop_dependency(file)
-		self._dependency_map[file]=True
+		if (file in self._dependency_map):
+			if (type(self._dependency_map[file])!=bool):
+				for pool_command in self._dependency_map[file]:
+					pool_command.drop_dependency(file)
+				self._dependency_map[file]=True
+		else:
+			self._dependency_map[file]=True
 		self._lock.release()
 
 	def wait(self):
@@ -87,7 +111,7 @@ class ProcessPool(object):
 		while (self._threads):
 			self._threads.pop().join()
 		for name in self._dependency_map.keys():
-			if (self._dependency_map[name] is not True):
+			if (type(self._dependency_map[name])!=bool):
 				sys.stdout.buffer.write(b"\x1b[1;91mUnresolved condition: "+bytes(name,"utf-8")+b"\x1b[0m\n")
 				sys.stdout.buffer.flush()
 				self._error=True
