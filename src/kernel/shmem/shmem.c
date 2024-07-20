def83f944be8ad56d022251c1884807ce741a805
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-07-20 08:51:06 +0200

    Push Update 20/07/2024, 08:51:06

--- src/kernel/shmem/shmem.c
+++ src/kernel/shmem/shmem.c
@@ -11,14 +11,35 @@
 
 
 
+static omm_allocator_t* KERNEL_INIT_WRITE _shmem_parent_region_allocator=NULL;
+static omm_allocator_t* KERNEL_INIT_WRITE _shmem_region_allocator=NULL;
+static handle_id_t KERNEL_INIT_WRITE _shmem_parent_region_handle_type=0;
+static handle_id_t KERNEL_INIT_WRITE _shmem_region_handle_type=0;
+
+
+
 KERNEL_INIT(){
-	return;
+	_shmem_parent_region_allocator=omm_init("kernel.shmem.region.parent",sizeof(shmem_parent_region_t),8,4);
+	rwlock_init(&(_shmem_parent_region_allocator->lock));
+	_shmem_region_allocator=omm_init("kernel.shmem.region",sizeof(shmem_region_t),8,4);
+	rwlock_init(&(_shmem_region_allocator->lock));
+	_shmem_parent_region_handle_type=handle_alloc("kernel.shmem.region.parent",0,NULL);
+	_shmem_region_handle_type=handle_alloc("kernel.shmem.region",0,NULL);
 }
 
 
 
 KERNEL_PUBLIC shmem_parent_region_t* shmem_parent_region_create(void* ptr,u64 size,u32 flags){
-	panic("shmem_parent_region_create");
+	if ((((u64)ptr)|size)&(PAGE_SIZE-1)){
+		panic("shmem_parent_region_create: unaligned arguments");
+	}
+	shmem_parent_region_t* out=omm_alloc(_shmem_parent_region_allocator);
+	handle_new(_shmem_parent_region_handle_type,&(out->handle));
+	rwlock_init(&(out->lock));
+	out->flags=flags;
+	out->ptr=ptr;
+	out->size=size;
+	return out;
 }
 
 
