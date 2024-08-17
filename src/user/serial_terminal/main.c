a7512fda5345a9b9a69ffce23cbc1554427a76ed
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2024-08-17 14:48:35 +0200

    Push Update 17/08/2024, 14:48:35

--- src/user/serial_terminal/main.c
+++ src/user/serial_terminal/main.c
@@ -227,7 +227,7 @@ s64 main(u32 argc,const char*const* argv){
 		ctrl_fd=child_pipes[2];
 		thread=sys_thread_create(_control_thread,NULL,NULL);
 		sys_thread_set_name(thread,"serial_terminal.control");
-		sys_thread_set_priority(thread,SYS_THREAD_PRIORITY_REALTIME);
+		sys_thread_set_priority(thread,SYS_THREAD_PRIORITY_HIGH);
 	}
 	sys_signal_set_mask(1<<SYS_SIGNAL_INTERRUPT,1);
 	sys_process_t process=sys_process_start(argv[first_argument_index],argc-first_argument_index,argv+first_argument_index,NULL,0,stdin,stdout_stderr,stdout_stderr);
